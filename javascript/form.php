22b63b2dd83703d738a64d845f3da52a86390f35
HRKISHACKED <ishakghimire3@gmail.com>
2024-12-27 08:12:37 +0545

    first

---
+++ javascript/form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FORM</title>
+</head>
+<body>
+	<form action ="result.php" method="POST">
+		<label>num1</label>
+		<input type="number" name="num1">
+		<label>num2</label>
+		<input type="number" name="num2">
+		<button type="submit">RESULT</button>
+	</form>
+</body>
+</html>
---
+++ javascript/result.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Result</title>
+</head>
+<body>
+	<? php
+	$num1=$_POST["num1"]
+	$num2=$_POST["num2"]
+	$result=$num1 + $num2;
+	echo $result;
+	?>
+
+
+</body>
+</html>
